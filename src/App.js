884c0f8ccf5af866b3ab7c338a9fb2e6146890c8
Willem Browne <willem.browne@gmail.com>
2018-10-04 16:08:33 +0200

    Added handling categories

--- src/App.js
+++ src/App.js
@@ -6,26 +6,41 @@ import Logo from './logo';
 import './App.css';
 
 const client = algoliasearch(process.env.INFERNO_APP_ALGOLIA_APPLICATION_ID, process.env.INFERNO_APP_ALGOLIA_API_KEY);
-const helper = algoliasearchHelper(client, 'apps');
+const helper = algoliasearchHelper(client, 'apps', {
+  facets: ['category']
+});
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: '',
-      apps: []
+      apps: [],
+      facets: [],
+      categories: [],
+      category: 'hello',
+      query: ''
     };
   }
 
   componentDidMount() {
     helper.on('result', (content) => {
-      this.setState({ apps: content.hits });
+      console.log('apps', content.hits);
+      console.log('facets', content.getFacetValues('category'));
+      this.setState({
+        apps: content.hits,
+        categories: [...new Set(content.hits.map(h => h.category))],
+        facets: content.getFacetValues('category')
+      });
     });
   }
 
   handleChange(event) {
     this.setState({ query: event.target.value });
     helper.setQuery(this.state.query).search();
+  }
+
+  handleCheck(event) {
+    helper.toggleFacetRefinement('category', event.target.name).search();
   }
 
   render() {
@@ -36,11 +51,25 @@ class App extends Component {
           <h1>Apps directory</h1>
         </header>
         <input type="text" value={this.state.query} onInput={(e) => this.handleChange(e)}/>
+        <aside>
+          {this.state.facets.map(facet => {
+            return (
+              <label>
+                {facet.name} [{facet.count}]
+                <input
+                  name={facet.name}
+                  type="checkbox"
+                  checked={facet.isRefined}
+                  onChange={(e) => this.handleCheck(e)} />
+              </label>
+            );
+          })}
+        </aside>
         <main>
           <ul>
             {this.state.apps.map(app => {
               return (
-                <li><span dangerouslySetInnerHTML={{__html: app._highlightResult.name.value}}/></li>
+                <li key={app.objectID}><span dangerouslySetInnerHTML={{__html: app._highlightResult.name.value}}/></li>
               );
             })}
           </ul>
